fc0a7ae9dbbe66b3f0281a71e001f4fbc75b4344
antar <av4004@rit.edu>
2016-01-30 17:18:13 -0500

    Initial Commit 2

---
+++ AppleAlarmWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  AppleAlarmWatch Extension
+//
+//  Created by Gabriel Mara Isserlis on 1/30/16.
+//  Copyright © 2016 Ankar Vasi, Alexander McLean, Gabriel Isserlis. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
