e9eb7b862bdc7ac87b375124af2443133bf10bb6
Alex McLean <adm9958@g.rit.edu>
2016-01-30 12:33:11 -0500

    Created new class Alarm.swift to represent the model.

---
+++ AppleAlarm/Alarm.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm.swift
+//  AppleAlarm
+//
+//  Created by Alexander McLean on 1/30/16.
+//  Copyright © 2016 Antar Vasi. All rights reserved.
+//
+
+import Foundation
+
+class Alarm : NSObject {
+    var time: NSDate
+    
+    override init() {
+        self.time = NSDate()
+    }
+}
